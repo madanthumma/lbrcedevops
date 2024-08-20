f41a44f3ff44ac1891ff1ee6e56812bfe308d398
AdmiN <AdmiN@ITWS068>
2024-08-20 10:55:51 +0530

    commite

--- src/lbrcedevops/leafyear.java
+++ src/lbrcedevops/leafyear.java
@@ -1,10 +1,9 @@
 package lbrcedevops;
 
-public class welcome {
+public class leafyear {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("welcome to Devops lab");
 
 	}
 
