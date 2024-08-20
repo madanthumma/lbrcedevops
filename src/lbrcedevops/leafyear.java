fde501a008ee487f4e9be1180a8db40643ef763f
AdmiN <AdmiN@ITWS068>
2024-08-20 10:59:28 +0530

    commite

--- src/lbrcedevops/leafyear.java
+++ src/lbrcedevops/leafyear.java
@@ -4,6 +4,16 @@ public class leafyear {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int year = 2020;
+
+	     if (year % 400 == 0)
+	       System.out.println (year + " is a Leap Year");
+
+	     else if (year % 4 == 0 && year % 100 != 0)
+	       System.out.println (year + " is a Leap Year");
+
+	     else
+	         System.out.println (year + " is not a Leap Year");
 
 	}
 
